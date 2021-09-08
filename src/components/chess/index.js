20776c1260ac9769278b334cfbfb830c44a1680f
William Chen <seed45699@gmail.com>
2021-09-08 22:37:36 +0800

    Update chess component

--- src/components/chess/index.js
+++ src/components/chess/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
-import { Square, BlackChess, WhiteChess } from './style';
+import { Square, BlackChess, WhiteChess, TransparentChess } from './style';
 
 function Chess(props) {
-  const SelectedChess = props.isBlack ? <BlackChess /> : <WhiteChess />;
+  const renderChess = () => {
+    let SelectedChess;
+    if (props.color === 'white') {
+      SelectedChess = <WhiteChess onClick={props.onClick} />;
+    } else if (props.color === 'black') {
+      SelectedChess = <BlackChess onClick={props.onClick} />;
+    } else {
+      SelectedChess = <TransparentChess onClick={props.onClick} />;
+    }
+    return SelectedChess;
+  };
+
   return (
     <Square rowIdx={props.rowIdx} colIdx={props.colIdx}>
-      {SelectedChess}
+      {renderChess()}
     </Square>
   );
 }
