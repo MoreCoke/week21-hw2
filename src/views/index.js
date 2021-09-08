c634cdbb4aa77adace706455761ac06139a9362f
William Chen <seed45699@gmail.com>
2021-09-08 22:38:41 +0800

    Feat Checkboard hook

--- src/views/index.js
+++ src/views/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 
 import { Container, Checkerboard, Row } from './style';
 import Chess from '../components/chess';
 
 function App() {
-  const foo = new Array(20).fill(new Array(20).fill(null));
+  const [game, setGame] = useState(new Array(20).fill(new Array(20).fill(null)));
+  const [chessColor, setChessColor] = useState('black');
+  const updateGame = useCallback((r, c, color) => {
+    const newGame = game.map((row, rowIdx) => {
+      if (rowIdx === r) {
+        const newRow = row.map((col, colIdx) => {
+          if (colIdx === c && col === null) {
+            return color;
+          }
+          return col;
+        });
+        return newRow;
+      }
+      return row;
+    });
+    setGame(newGame);
+  });
+  const updateChessColor = useCallback(() => {
+    if (chessColor === 'black') {
+      setChessColor('white');
+      return;
+    }
+    setChessColor('black');
+  });
+
   return (
     <Container>
       <Checkerboard>
-        {foo.map((row, rowIndex) => (
+        {game.map((row, rowIndex) => (
           <Row key={`row-${rowIndex}`}>
             {row.map((col, colIndex) => (
-              <Chess rowIdx={rowIndex} colIdx={colIndex} key={`col-${colIndex}`} />
+              <Chess
+                rowIdx={rowIndex}
+                colIdx={colIndex}
+                key={`col-${colIndex}`}
+                onClick={() => {
+                  updateGame(rowIndex, colIndex, chessColor);
+                  updateChessColor();
+                }}
+                color={col}
+              />
             ))}
           </Row>
         ))}
